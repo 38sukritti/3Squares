7f2265b4e9a03636d9b04db45c490535f6088951
38sukritti <75665543+38sukritti@users.noreply.github.com>
2026-04-16 15:07:18 +0530

    Fix requirements.txt encoding to UTF-8 and fix djangorestframework name

--- dubai_project/settings.py
+++ dubai_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&zkgowc$he!eyk-_!r__40+k35q*zur(li=)8x$pc(g&6t90ck'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'threesquares.onrender.com',
